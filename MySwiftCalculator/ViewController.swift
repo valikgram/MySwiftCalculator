138707d6cd63b0d64013db45319eb658f0e32086
Dzmitry Valkovich <valikgram@gmail.com>
2021-10-18 02:05:13 +0300

    Initial Commit

---
+++ MySwiftCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MySwiftCalculator
+//
+//  Created by Дмитрий Валькович on 18.10.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
