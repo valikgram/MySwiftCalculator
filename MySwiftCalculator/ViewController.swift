b544b15c21d3ba922559ae797e660b6d2f0bf04b
Dzmitry Valkovich <valikgram@gmail.com>
2021-10-18 02:11:07 +0300

    Added UI without functional

--- MySwiftCalculator/ViewController.swift
+++ MySwiftCalculator/ViewController.swift
@@ -1,19 +1,53 @@
 //
 //  ViewController.swift
-//  MySwiftCalculator
+//  Calculator
 //
-//  Created by Дмитрий Валькович on 18.10.21.
+//  Created by Дмитрий Валькович on 17.10.21.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var zeroButton: RoundButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+//        zeroButton.layer.cornerRadius = 40
     }
-
-
+    
+    
+    
 }
 
+@IBDesignable class RoundButton: UIButton {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        sharedInit()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        sharedInit()
+    }
+
+    override func prepareForInterfaceBuilder() {
+        sharedInit()
+    }
+
+    func sharedInit() {
+        refreshCorners(value: cornerRadius)
+    }
+
+    func refreshCorners(value: CGFloat) {
+        layer.cornerRadius = value
+    }
+
+    var cornerRadius: CGFloat = 15 {
+        didSet {
+            refreshCorners(value: cornerRadius)
+        }
+    }
+
+}
